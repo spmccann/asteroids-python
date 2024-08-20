827ff03dfef92f122c03099a69f59602d220b5e2
spmccann <scottpmccann@gmail.com>
2024-08-20 06:23:38 -0700

    player movement, groups

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT /2) 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
+        player.update(dt)
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
-        print("Starting asteroids!")
-        print("Screen width:", SCREEN_WIDTH) 
-        print("Screen height:", SCREEN_HEIGHT)
+
+        dt = clock.tick(60)  / 1000
 
 if __name__ == "__main__":
     main()
+
+
