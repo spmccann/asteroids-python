39e6ab11b261e76b36740dd207e86abc1fcac7bc
spmccann <scottpmccann@gmail.com>
2024-08-20 03:08:44 -0700

    game loop, black screen

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  
+        pygame.Surface.fill(screen, color="black")
+        pygame.display.flip()
+        print("Starting asteroids!")
+        print("Screen width:", SCREEN_WIDTH) 
+        print("Screen height:", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
